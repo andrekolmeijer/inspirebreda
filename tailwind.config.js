9e7f3296b191da8177013a2c8ddb281b76a6960c
ndrklmr <andrekolmeijer@gmail.com>
2021-11-04 09:53:21 +0100

    Added fonts and brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'inspire-orange': '#F98A0C',
+        'inspire-orange': '#F98A0C', // #f28c00
         'inspire-brown': '#331606',
         'inspire-brown-light': '#DDC5B8',
       },
       fontFamily: {
         sans: 'Roboto, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
-        'condensed': ['"Roboto Condensed"']
+        'condensed': '"Roboto Condensed"',
+        'oswald': 'Oswald'
       },
     },
   },
